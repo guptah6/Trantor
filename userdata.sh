4ab1a8ac5e73f45624391cdb1466bad8b7cc9838
guptah6 <83236445+guptah6@users.noreply.github.com>
2023-11-02 15:10:07 +0530

    Add files via upload

---
+++ userdata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo su
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+docker pull nginxdemos/hello
+docker run -P -d nginxdemos/hello
+
